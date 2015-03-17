16d20ae609432f417753fbc6ff74c924edef8539
= <zacfirst@gmail.com>
2015-03-17 02:13:24 +0000

    + Static fixture test

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -47,4 +47,16 @@ class ProductTest < ActiveSupport::TestCase
       assert new_product(name).invalid?, "#{name} shoudn't be valid"
     end
   end
+  
+  test "product is not valid without a unique title" do
+    product = Product.new(:title => products(:ruby).title,
+                :description => "description",
+                :price => 1,
+                :image_url => "fred.jpg")
+    assert !product.save
+    assert_equal "has already been taken", product.errors[:title].join(';')
+    assert_equal I18n.translate('activerecord.errors.messages.taken'), 
+                                product.errors[:title].join(';')
+
+    end
 end
