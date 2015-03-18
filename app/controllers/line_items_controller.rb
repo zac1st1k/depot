5a4cab48220ea801841793e952bf6df6e6f595c1
= <zacfirst@gmail.com>
2015-03-18 04:56:51 +0000

    + Line item remove

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -81,7 +81,7 @@ class LineItemsController < ApplicationController
     @line_item.destroy
 
     respond_to do |format|
-      format.html { redirect_to(line_items_url) }
+      format.html { redirect_to @line_item.cart, :notice => "Item has been removed from your cart." }
       format.xml  { head :ok }
     end
   end
