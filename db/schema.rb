9d6b35bfdc773d0eae726702547dcbf8bd65b1eb
= <zacfirst@gmail.com>
2015-03-18 01:55:06 +0000

    + Line item grouping and db rollback

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150317054903) do
+ActiveRecord::Schema.define(:version => 20150318013317) do
 
   create_table "carts", :force => true do |t|
     t.datetime "created_at"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(:version => 20150317054903) do
     t.integer  "cart_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "quantity",   :default => 1
   end
 
   create_table "products", :force => true do |t|
