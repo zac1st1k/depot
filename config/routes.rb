c5a9bad1def9d339d750ebbc77afcaab95460bfd
= <zacfirst@gmail.com>
2015-03-19 07:01:54 +0000

    + Atom notification

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Depot::Application.routes.draw do
-  root :to => "store#index", :as => 'store'
   
   resources :orders
 
@@ -14,7 +13,11 @@ Depot::Application.routes.draw do
   get "say/hello"
 
   get "say/goodbye"
-
+  
+  resources :products do
+    get :who_bought, :on => :member
+  end
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -64,7 +67,7 @@ Depot::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "store#index", :as => 'store'
+  root :to => "store#index", :as => 'store'
 
   # See how all your routes lay out with "rake routes"
 
