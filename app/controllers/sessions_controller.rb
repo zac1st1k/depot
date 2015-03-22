aeb092f3015654680b6fa2a5f9336c4c2a7bd13a
= <zacfirst@gmail.com>
2015-03-22 04:48:53 +0000

    + User test

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,15 +5,15 @@ class SessionsController < ApplicationController
   def create
     if user = User.authenticate(params[:name], params[:password])
       session[:user_id] = user.id
-      # redirect_to admin_url
-      redirect_to orders_path
+      redirect_to admin_url
+      # redirect_to orders_path
 
     else
       redirect_to login_url, :alert => "Invalid user/password combination"
     end
   end
 
-  def destory
+  def destroy
     session[:user_id] = nil
     redirect_to store_url, :notice => "Logged out"
   end
