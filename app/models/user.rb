4f918da9d2f86b2afb77299f1c2ecadbfcabdfbd
= <zacfirst@gmail.com>
2015-03-23 09:35:23 +0000

    + Rollback transaction

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,16 @@
 class User < ActiveRecord::Base
+  after_destroy :ensure_an_admin_remains
   validates :name, :presence => true, :uniqueness => true
   validates :password, :confirmation => true
   attr_accessor :password_confirmation
   attr_reader :password
   validate :password_must_be_present
+  
+  def ensure_an_admin_remains
+    if User.count.zero?
+      raise "Can't delete last user"
+    end
+  end
   
   def User.authenticate(name,password)
     if user = find_by_name(name)
