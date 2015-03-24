38c5511eb2201e4ae7fdd71f4635d47183345e3f
= <zacfirst@gmail.com>
2015-03-24 10:22:57 +0000

    + Language switch

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,12 +2,17 @@ class StoreController < ApplicationController
   skip_before_filter :authorize
   
   def index
-    @products = Product.all
-    @cart = current_cart
     
-    if session[:counter].nil?
-      session[:counter] = 0
+    if params[:set_locale]
+      redirect_to store_path(:locale => params[:set_locale])
+    else
+      @products = Product.all
+      @cart = current_cart
+      
+      if session[:counter].nil?
+        session[:counter] = 0
+      end
+      session[:counter] += 1
     end
-    session[:counter] += 1
   end
 end
